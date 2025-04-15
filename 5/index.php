e4343c8b8142e525ec0db27ec6a796ced8ec0d99
XiJiaHui <xjh@sinosoft.com.cn>
2025-04-15 11:25:42 +0800

    update 5.2.0

--- 5/index.php
+++ 5/index.php
@@ -1,55 +1,59 @@
 <?php
 namespace docker {
-	final class DefaultServerPlugin {
-		public function __construct(
-			private \AdminerPlugin $adminer
-		) { }
-
-		public function loginFormField(...$args) {
-			return (function (...$args) {
-				$field = \Adminer\Adminer::loginFormField(...$args);
-	
-				$filePath = "/usr/local/oracle/instantclient_19_26/network/admin/tnsnames.ora";
-				if (file_exists($filePath) && is_file($filePath)) {
-					$pattern = '/<select name=\'auth\[driver\]\'>.*?<\/select>/s';
-					$replacement = '<select name=\'auth[driver]\'><option value="oracle" selected> Oracle    (beta) </select>';
-					$field = preg_replace($pattern, $replacement, $field);	
-					
-					$pattern = '/name="auth\[db\]"\s+value="[^"]*"/';
-					$replacement = 'name="auth[db]" value="" readonly';
-					$field = preg_replace($pattern, $replacement, $field);
-				}
-
-				if (!empty($_ENV['ORACLE_SID'])) {
-					if (preg_match('/name="auth\[server\]"[^>]*title="hostname\[:port\]"/', $field, $matches)) {
-						$field = str_replace($matches[0], 'name="auth[server]" value="'.($_ENV['ORACLE_SID']).'" readonly title="hostname[:port]"', $field);
-					}
-				}
-				
-				return \str_replace(
-					'name="auth[server]" value="" title="hostname[:port]"',
-					\sprintf('name="auth[server]" value="%s" title="hostname[:port]"', ($_ENV['ADMINER_DEFAULT_SERVER'] ?: 'db')),
-					$field,
-				);
-			})->call($this->adminer, ...$args);
-		}
-	}
-
 	function adminer_object() {
-		require_once('plugins/plugin.php');
+		/**
+		 * Prefills the “Server” field with the ADMINER_DEFAULT_SERVER environment variable.
+		 */
+		final class DefaultServerPlugin extends \Adminer\Plugin {
+			public function __construct(
+				private \Adminer\Adminer $adminer
+			) { }
+
+			public function loginFormField(...$args) {
+				return (function (...$args) {
+					$field = $this->loginFormField(...$args);
+
+                    $filePath = "/usr/local/oracle/instantclient_19_26/network/admin/tnsnames.ora";
+                    if (file_exists($filePath) && is_file($filePath)) {
+                        $pattern = '/<select name=\'auth\[driver\]\'>.*?<\/select>/s';
+                        $replacement = '<select name=\'auth[driver]\'><option value="oracle" selected> Oracle    (beta) </select>';
+                        $field = preg_replace($pattern, $replacement, $field);
+
+                        $pattern = '/name="auth\[db\]"\s+value="[^"]*"/';
+                        $replacement = 'name="auth[db]" value="" readonly';
+                        $field = preg_replace($pattern, $replacement, $field);
+                    }
+
+                    if (!empty($_ENV['ORACLE_SID'])) {
+                        if (preg_match('/name="auth\[server\]"[^>]*title="hostname\[:port\]"/', $field, $matches)) {
+                            $field = str_replace($matches[0], 'name="auth[server]" value="'.($_ENV['ORACLE_SID']).'" readonly title="hostname[:port]"', $field);
+                        }
+                    }
+
+					return \str_replace(
+						'name="auth[server]" value="" title="hostname[:port]"',
+						\sprintf('name="auth[server]" value="%s" title="hostname[:port]"', ($_ENV['ADMINER_DEFAULT_SERVER'] ?: 'db')),
+						$field,
+					);
+				})->call($this->adminer, ...$args);
+			}
+		}
 
 		$plugins = [];
 		foreach (glob('plugins-enabled/*.php') as $plugin) {
 			$plugins[] = require($plugin);
 		}
 
-		// Load the DefaultServerPlugin last to give other plugins a chance to
-		// override loginFormField() if they wish to.
-		$plugins[] = &$loginFormPlugin;
+		$adminer = new \Adminer\Plugins($plugins);
 
-		$adminer = new \AdminerPlugin($plugins);
-
-		$loginFormPlugin = new DefaultServerPlugin($adminer);
+		(function () {
+			$last = &$this->hooks['loginFormField'][\array_key_last($this->hooks['loginFormField'])];
+			if ($last instanceof \Adminer\Adminer) {
+				$defaultServerPlugin = new DefaultServerPlugin($last);
+				$this->plugins[] = $defaultServerPlugin;
+				$last = $defaultServerPlugin;
+			}
+		})->call($adminer);
 
 		return $adminer;
 	}
